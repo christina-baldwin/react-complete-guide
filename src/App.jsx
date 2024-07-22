b375a1668880c4e722b6fcfe5489488f79630d6d
christina-baldwin <christina.baldwin13@yahoo.com>
2024-07-22 10:24:45 +0200

    Implementing active player change and player symbol placement

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import { useState } from "react";
+import Log from "./components/Log";
 
 function App() {
+  // manage the infomration for the different button clicks here
+  const [gameTurns, setGameTurns] = useState([]);
+  // this player with the symbol X is the active player initially
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  //function activatged whenever we want to switch turns
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { sqaure: { row: rowIndex, col: colIndex }, player: currPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         PLAYERS
         {/* ol because the order matters */}
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
+      <Log />
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,15 +4,32 @@ const initalGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare }) {
+  /*
+  // switch this data to be in the App in the gameTurns state to avoid intersecting states
+  // state to manage and update the game board
+  const [gameBoard, setGameBoard] = useState(initalGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      // this gives a brand new array with brand new nested arrays and updates the board in an immutable way
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }*/
   return (
     <ol id="game-board">
-      {initalGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={onSelectSquare}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
