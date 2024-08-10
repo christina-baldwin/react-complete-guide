1b195056316d9c0206514fa2d654244b4e735821
christina-baldwin <christina.baldwin13@yahoo.com>
2024-08-10 11:18:50 +0200

    http error handling, transforming data, sending data with ost requests, optimistic updating

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,29 +1,54 @@
 import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 // this doesnt work when getting data from a backend
 // const places = localStorage.getItem();
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  // loading state
   const [isFetching, setIsFetching] = useState(false);
+  // data state
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  // error state
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      if (!response.ok) {
-        const error = new Error("Failed to fetch places");
-        throw error;
-      }
 
-      setAvailablePlaces(resData.places);
-      setIsFetching(false);
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          // with the above position call this will execute too soon so need to move it to the position callback function
+          setIsFetching(false);
+        });
+      } catch (error) {
+        // what is executed if an error is encountered
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+        // set after the error as well
+        setIsFetching(false);
+      }
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
   // this creates an infinite loop because it is in the component function is it would be re-executed every time the component re-executes so need to use useEffect
   //   fetch("http://localhost:3000/places")
   //     .then((response) => {
---
+++ src/http.js
@@ -0,0 +1,31 @@
+// this file is to add a sperate function for the code that fetches the place data (optional)
+export async function fetchAvailablePlaces() {
+  const response = await fetch("http://localhost:3000/places");
+  const resData = await response.json();
+  if (!response.ok) {
+    const error = new Error("Failed to fetch places");
+    throw error;
+  }
+
+  return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    // backend expects an array with a "places" key
+    body: JSON.stringify({ places: places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data!");
+  }
+
+  // backend code contains an object with a message property
+  return resData.message;
+}
