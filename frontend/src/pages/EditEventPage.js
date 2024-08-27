d1ff7074da38dcb7edb3f0ade69ebcb315c78c71
christina-baldwin <christina.baldwin13@yahoo.com>
2024-08-27 15:23:56 +0200

    accessing data from other routes using useRouteLoaderData()

--- frontend/src/pages/EditEventPage.js
+++ frontend/src/pages/EditEventPage.js
@@ -1,14 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
 
 function EditEventPage() {
-  const params = useParams();
+  const data = useRouteLoaderData("event-detail");
 
-  return (
-    <>
-      <h1>Edit Event Page</h1>
-      <p>{params.eventId}</p>
-    </>
-  );
+  return <EventForm event={data.event} />;
 }
 
 export default EditEventPage;
