50fcdf294314465cf7cb2b1086922de390c8f043
christina-baldwin <christina.baldwin13@yahoo.com>
2024-09-10 16:24:30 +0200

    working with base types

---
+++ basics.ts
@@ -0,0 +1,24 @@
+//Primitives
+let age: number = 10;
+
+age = 12;
+
+let userName: string = "Max";
+
+let isInstructor: boolean = true;
+
+//Complex types
+
+let hobbies: string[];
+
+let ages: number[];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
